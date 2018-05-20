d3b2e0bece4bdf7336877c0878ec732e747dc32c
早稲田風太 <>
2018-05-20 14:51:54 +0900

    途中。（ルートが表示されない、ピン留されない）

--- tripmap/tripmap.js
+++ tripmap/tripmap.js
@@ -5,7 +5,7 @@
 // This example requires the Places library. Include the libraries=places
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-var places;
+
 function initAutocomplete() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -33.8688, lng: 151.2195},
@@ -15,32 +15,75 @@ function initAutocomplete() {
 
   new AutocompleteDirectionsHandler(map);
 
+  // // Create the search box and link it to the UI element.
+  // var input = document.getElementById('pac-input');
+  // var searchBox = new google.maps.places.SearchBox(input);
+  // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+
+  document.getElementById('submit').addEventListener('click', function() {
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    new calculateAndDisplayRoute(directionsService,directionsDisplay);
+    console.log("aaa");
+  });
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+
+}
+/**/
+
+var originPlaceId;
+var destinationPlaceId;
+var travelMode;
+var markers = [];
+function AutocompleteDirectionsHandler(map) {
+  this.map = map;
+  originPlaceId = null;
+  destinationPlaceId = null;
+  travelMode = 'WALKING';
+
+  var originInput = document.getElementById('origin-input');
+  var destinationInput = document.getElementById('destination-input');
+  var modeSelector = document.getElementById('mode-selector');
+  this.directionsService = new google.maps.DirectionsService;
+  this.directionsDisplay = new google.maps.DirectionsRenderer;
+  this.directionsDisplay.setMap(map);
+
+  var originAutocomplete = new google.maps.places.Autocomplete(originInput);
+  var destinationAutocomplete = new google.maps.places.Autocomplete(destinationInput);
+
+  this.setupClickListener('changemode-walking', 'WALKING');
+  this.setupClickListener('changemode-transit', 'TRANSIT');
+  this.setupClickListener('changemode-driving', 'DRIVING');
+
+  this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
+  this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
+
+  this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+  this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
+  this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
+
   // Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
+  var input = document.getElementById('via-input');
   var searchBox = new google.maps.places.SearchBox(input);
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+
 
   // Bias the SearchBox results towards current map's viewport.
   map.addListener('bounds_changed', function() {
     searchBox.setBounds(map.getBounds());
   });
 
-  var directionsService = new google.maps.DirectionsService;
-  var directionsDisplay = new google.maps.DirectionsRenderer;
-  document.getElementById('submit').addEventListener('click', function() {
-    new AutocompleteDirectionsHandler(map);
-    console.log("aaa");
-  });
-
-  var markers = [];
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
   searchBox.addListener('places_changed', function() {
-    places = searchBox.getPlaces();
+    var places = searchBox.getPlaces();
 
     //クリップボードに表示
-    var option_element = document.createElement("option");
-    option_element.innerHTML = '<option value='+places[0].formatted_address+'>'+places[0].name+'</option>';
+    var option_element=document.createElement("option") ;
+    var txt=document.createTextNode(places[0].name);
+    option_element.id=places[0].formatted_address;
+    option_element.appendChild(txt);
     var parent_object = document.getElementById("waypoints");
     parent_object.appendChild(option_element);
 
@@ -49,13 +92,10 @@ function initAutocomplete() {
     if (places.length == 0) {
       return;
     }
-
     // // Clear out the old markers.
     // markers.forEach(function(marker) {
     //   marker.setMap(null);
     // });
-
-    markers = [];
 
     // For each place, get the icon, name and location.
     var bounds = new google.maps.LatLngBounds();
@@ -64,18 +104,18 @@ function initAutocomplete() {
         console.log("Returned place contains no geometry");
         return;
       }
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
+      // var icon = {
+      //   url: place.icon,
+      //   size: new google.maps.Size(71, 71),
+      //   origin: new google.maps.Point(0, 0),
+      //   anchor: new google.maps.Point(17, 34),
+      //   scaledSize: new google.maps.Size(25, 25)
+      // };
 
       // Create a marker for each place.
       markers.push(new google.maps.Marker({
         map: map,
-        icon: icon,
+        // icon: icon,
         title: place.name,
         position: place.geometry.location
       }));
@@ -90,38 +130,9 @@ function initAutocomplete() {
     map.fitBounds(bounds);
 
   });
-}
-/**/
-
-function AutocompleteDirectionsHandler(map) {
-  this.map = map;
-  this.originPlaceId = null;
-  this.destinationPlaceId = null;
-  this.travelMode = 'WALKING';
-  var originInput = document.getElementById('origin-input');
-  var destinationInput = document.getElementById('destination-input');
-  var modeSelector = document.getElementById('mode-selector');
-  this.directionsService = new google.maps.DirectionsService;
-  this.directionsDisplay = new google.maps.DirectionsRenderer;
-  this.directionsDisplay.setMap(map);
-
-  var originAutocomplete = new google.maps.places.Autocomplete(
-     originInput, {placeIdOnly: true});
-  var destinationAutocomplete = new google.maps.places.Autocomplete(
-     destinationInput, {placeIdOnly: true});
-
-  this.setupClickListener('changemode-walking', 'WALKING');
-  this.setupClickListener('changemode-transit', 'TRANSIT');
-  this.setupClickListener('changemode-driving', 'DRIVING');
-
-  this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
-  this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
-
-  this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
-  this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
-  this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
   console.log("AutocompleteDirectionsHandler終わり");
 }
+
 
 // Sets a listener on a radio button to change the filter type on Places
 // Autocomplete.
@@ -129,8 +140,8 @@ AutocompleteDirectionsHandler.prototype.setupClickListener = function(id, mode) 
  var radioButton = document.getElementById(id);
  var me = this;
  radioButton.addEventListener('click', function() {
-   me.travelMode = mode;
-   me.route();
+   travelMode = mode;
+   // me.route();
  });
  console.log("setupClickListener終わり");
 };
@@ -144,60 +155,104 @@ AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(aut
      window.alert("Please select an option from the dropdown list.");
      return;
    }
+   console.log(place);
+   // // Create a marker for each place.
+   // markers.push(new google.maps.Marker({
+   //   map: this.map,
+   //   // icon: icon,
+   //   title: place.name,
+   //   position: place.geometry.location
+   // }));
    if (mode === 'ORIG') {
-     me.originPlaceId = place.place_id;
+     originPlaceId = place.place_id;
    } else {
-     me.destinationPlaceId = place.place_id;
+     destinationPlaceId = place.place_id;
    }
-   me.route();
+   // me.route();
  });
  console.log("setupPlaceChangedListener終わり");
 };
 
+//
+// AutocompleteDirectionsHandler.prototype.route = function () {
+//  if (!originPlaceId || !destinationPlaceId) {
+//    return;
+//    console.log("naiyo");
+//  }
+//  var me = this;
+//
+//  this.directionsService.route({
+//    origin: {'placeId': originPlaceId},
+//    destination: {'placeId': destinationPlaceId},
+//    travelMode: travelMode
+//  }, function(response, status) {
+//    if (status === 'OK') {
+//      me.directionsDisplay.setDirections(response);
+//      var route = response.routes[0];
+//      var summaryPanel = document.getElementById('directions-panel');
+//      summaryPanel.innerHTML = '';
+//      // For each route, display summary information.
+//      for (var i = 0; i < route.legs.length; i++) {
+//        var routeSegment = i + 1;
+//        summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+//            '</b><br>';
+//        summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+//        summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+//        summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+//      }
+//    } else {
+//      window.alert('Directions request failed due to ' + status);
+//    }
+//  });
+//
+//  console.log("route終わり");
+// };
 
-AutocompleteDirectionsHandler.prototype.route = function () {
- if (!this.originPlaceId || !this.destinationPlaceId) {
-   return;
-   console.log("naiyo");
- }
- var me = this;
 
- var waypts = [];
- var checkboxArray = document.getElementById('waypoints');
- for (var i = 0; i < checkboxArray.length; i++) {
-  if (checkboxArray.options[i].selected) {
-    waypts.push({
-      location: checkboxArray[i].value,
-      stopover: true
-    });
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  console.log('waypts前');
+  var waypts=[];
+  var checkboxArray = document.getElementById('waypoints');
+  for (var i = 0; i < checkboxArray.length; i++) {
+    if (checkboxArray.options[i].selected) {
+      console.log('wayptsあった'+checkboxArray[i].id);
+      waypts.push({
+      　location: checkboxArray[i].id,
+        stopover: true
+      });
+    }
   }
- }
- console.log("waypts="+waypts);
 
- this.directionsService.route({
-   origin: {'placeId': this.originPlaceId},
-   destination: {'placeId': this.destinationPlaceId},
-   waypoints: waypts,
-   travelMode: this.travelMode
- }, function(response, status) {
-   if (status === 'OK') {
-     me.directionsDisplay.setDirections(response);
-     var route = response.routes[0];
-     var summaryPanel = document.getElementById('directions-panel');
-     summaryPanel.innerHTML = '';
-     // For each route, display summary information.
-     for (var i = 0; i < route.legs.length; i++) {
-       var routeSegment = i + 1;
-       summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
-           '</b><br>';
-       summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
-       summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
-       summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
-     }
-   } else {
-     window.alert('Directions request failed due to ' + status);
-   }
- });
-
- console.log("route終わり");
-};
+  directionsService.route({
+    origin: {'placeId': originPlaceId},
+    destination: {'placeId': destinationPlaceId},
+    waypoints: waypts,
+    optimizeWaypoints: true,
+    travelMode: travelMode
+  }, function(response, status) {
+    console.log(originPlaceId+" "+destinationPlaceId+" "+waypts);
+    if (status === 'OK') {
+      /*marker delete*/
+      markers.forEach(function(marker) {
+        marker.setMap(null);
+      });
+      /**/
+      console.log(response);
+      directionsDisplay.setDirections(response);
+      var route = response.routes[0];
+      var summaryPanel = document.getElementById('directions-panel');
+      summaryPanel.innerHTML = '';
+      // For each route, display summary information.
+      for (var i = 0; i < route.legs.length; i++) {
+        var routeSegment = i + 1;
+        summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+            '</b><br>';
+        summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+        summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+        summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+      }
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+}
