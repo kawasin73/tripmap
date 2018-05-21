e9d20ba9c4f13d14db7189cb4e1262ae769b06c0
川辺航 <>
2018-05-21 13:21:55 +0900

    no message

--- tripmap-wtr/tripmap.js
+++ tripmap-wtr/tripmap.js
@@ -14,9 +14,9 @@ function initAutocomplete() {
   });
 
   var default_lat = [35.717329, 35.699691];
-  var default_lng = [139.736772, 139.764765];
+  var default_lon = [139.736772, 139.764765];
   for (i=0; i<default_lat.length; i++){
-   var myLatlng = new google.maps.LatLng(default_lat[i], default_lng[i]);
+   var myLatlng = new google.maps.LatLng(default_lat[i], default_lon[i]);
    var default_marker = new google.maps.Marker({
        position: myLatlng,
        title:"Hello World!"
