6cfb90ed479ae766cd1dce256b2eb90d17b1bdec
早稲田風太 <>
2018-05-22 22:27:26 +0900

    header,checkboxなどのデザイン(html,css,js)

--- static/scripts/tripmap.js
+++ static/scripts/tripmap.js
@@ -5,7 +5,6 @@
 // This example requires the Places library. Include the libraries=places
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
 function initAutocomplete() {
   var directionsService = new google.maps.DirectionsService;
   var directionsDisplay = new google.maps.DirectionsRenderer;
@@ -61,7 +60,7 @@ function AutocompleteDirectionsHandler(map) {
   // Create the search box and link it to the UI element.
   var input = document.getElementById('via-input');
   var searchBox = new google.maps.places.SearchBox(input);
-  this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
 
   // Bias the SearchBox results towards current map's viewport.
@@ -74,14 +73,13 @@ function AutocompleteDirectionsHandler(map) {
     var places = searchBox.getPlaces();
 
     //クリップボードに表示
-    var option_element=document.createElement("option") ;
+    var label_element=document.createElement("label") ;
     var txt=document.createTextNode(places[0].name);
-    option_element.id=places[0].formatted_address;
-    option_element.appendChild(txt);
+    label_element.innerHTML="<input type='checkbox' name='checkbox01' class='checkbox01-input' value="+places[0].formatted_address+"><span class='checkbox01-parts'>"+places[0].name+"</span><br>";
     var parent_object = document.getElementById("waypoints");
-    parent_object.appendChild(option_element);
+    parent_object.appendChild(label_element);
 
-    console.log(places);
+    console.log(places[0].name);
 
     if (places.length == 0) {
       return;
@@ -109,6 +107,8 @@ function AutocompleteDirectionsHandler(map) {
       }
       map.fitBounds(bounds);
     });
+    var viapoint=document.getElementById('via-input');
+    viapoint.value=null;
 
   });
   console.log("AutocompleteDirectionsHandler終わり");
@@ -152,7 +152,7 @@ AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(map
     } else {
       bounds.extend(place.geometry.location);
     }
-
+    map.fitBounds(bounds);
 
     if (mode === 'ORIG') {
       originPlaceId = place.place_id;
@@ -168,26 +168,29 @@ AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(map
 //ルート計算
 function calculateAndDisplayRoute(directionsService, directionsDisplay) {
   if (!originPlaceId || !destinationPlaceId) {
+    alert("出発地または到着地を正しく選択して下さい");
     return;
   }
   var waypts=[];
-  var checkboxArray = document.getElementById('waypoints');
+  var checkboxArray=document.getElementsByClassName('checkbox01-input');
+  console.log(checkboxArray[0]);
   for (var i = 0; i < checkboxArray.length; i++) {
-    if (checkboxArray.options[i].selected) {
-      console.log('waypts:'+checkboxArray[i].id);
+    if (checkboxArray[i].checked) {
+      console.log('waypts:'+checkboxArray[i].value);
       waypts.push({
-        location: checkboxArray[i].id,
+        location: checkboxArray[i].value,
         stopover: true
       });
     }
   }
+
   directionsService.route({
     origin: {'placeId': originPlaceId},
     destination: {'placeId': destinationPlaceId},
     waypoints: waypts,
     optimizeWaypoints: true,
     travelMode: travelMode
-  }, function(response, status) {
+    }, function(response, status) {
     console.log(originPlaceId+" "+destinationPlaceId+" "+waypts);
     if (status === 'OK') {
       // /*marker delete*/
@@ -199,16 +202,16 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
       directionsDisplay.setDirections(response);
       var route = response.routes[0];
       var summaryPanel = document.getElementById('directions-panel');
-      summaryPanel.innerHTML = '';
+      summaryPanel.innerHTML = '<br><hr>';
       // For each route, display summary information.
       for (var i = 0; i < route.legs.length; i++) {
         var routeSegment = i + 1;
-        summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+        summaryPanel.innerHTML += '<b>区間: ' + routeSegment +
           '</b><br>';
-        summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+        summaryPanel.innerHTML += route.legs[i].start_address + ' から ';
         summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
-        summaryPanel.innerHTML += route.legs[i].distance.text + '<br>';
-        summaryPanel.innerHTML += route.legs[i].duration.text + '<br><br>';
+        summaryPanel.innerHTML += '<h4>距離：'+route.legs[i].distance.text + '</h4>';
+        summaryPanel.innerHTML += '<h4>時間：'+route.legs[i].duration.text + '</h4><hr>';
       }
     } else {
       window.alert('Directions request failed due to ' + status);
